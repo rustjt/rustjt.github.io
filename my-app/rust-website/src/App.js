75a5f9f3de37c4ae189804497e8d6c4100485c44
Jordan Rust <feo2inator@gmail.com>
2023-08-24 13:18:26 -0400

    added navbar css

--- my-app/rust-website/src/App.js
+++ my-app/rust-website/src/App.js
@@ -4,16 +4,19 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Projects from './pages/Projects';
 import Experience from './pages/Experience';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/" element={<Projects />} />
-          <Route path="/" element={<Experience />} />
-        </Routes>
+        <Navbar>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/experience" element={<Experience />} />
+          </Routes>
+        </Navbar>
       </Router>  
     </div>
   );
