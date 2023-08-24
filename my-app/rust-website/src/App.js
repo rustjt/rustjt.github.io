ba50d6b709283c98157fb69c923da48e46c8a900
Jordan Rust <feo2inator@gmail.com>
2023-08-24 13:05:26 -0400

    add imports to App.js

--- my-app/rust-website/src/App.js
+++ my-app/rust-website/src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Home from './pages/Home';
+import Projects from './pages/Projects';
+import Experience from './pages/Experience';
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Projects />} />
+          <Route path="/" element={<Experience />} />
+        </Routes>
+      </Router>  
     </div>
   );
 }
